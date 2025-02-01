fd3d74b834411b395e97f919c4c5a6b1baab8441
ccrawford4 <calumcrawford9@gmail.com>
2025-01-31 16:35:04 -0800

    added create + remove sctipt

--- python/create.py
+++ python/create.py
@@ -5,7 +5,7 @@ def create_ec2_instance():
     conn = ec2.run_instances(InstanceType="t2.micro",
                             MaxCount=1,
                             MinCount=1,
-                            ImageId="ami-02d55cb47e83a99a0")
+                            ImageId="ami-0c614dee691cbbf37")
     print("New EC2 Instance Created: ", conn['Instances'][0]['InstanceId'])
 
 if __name__ == "__main__":
--- python/remove.py
+++ python/remove.py
@@ -6,5 +6,5 @@ def create_ec2_instance(ec2_id):
     print(f"Response from AWS: {response} for instance ID: {ec2_id}")
 
 if __name__ == "__main__":
-    ec2_id = "i-0b3b3b3b3b3b3b3b3"
+    ec2_id = "i-07b55927e0a0b501c"
     create_ec2_instance(ec2_id=ec2_id)
